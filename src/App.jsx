6d8101f37949ca92d5a34585f4e50ec1c784d96e
Nosferatu721 <elkintorres721@gmail.com>
2023-03-24 22:54:11 -0500

    Finish LPM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { Container, Grid, Typography } from '@mui/material';
+import { NoticiasProvider } from './context/NoticiasProvider';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
-
   return (
-    <h1>App Noticias</h1>
-  )
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align="center" margin={5} component="h1" variant="h3">
+            Buscador de Noticias
+          </Typography>
+        </header>
+
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+          <Grid item lg={4} md={6} xs={12}>
+            <Formulario />
+          </Grid>
+        </Grid>
+
+        <ListadoNoticias />
+      </Container>
+    </NoticiasProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { useState, useEffect, createContext } from 'react';
+
+const NoticiasContext = createContext();
+
+const NoticiasProvider = ({ children }) => {
+  const [categoria, setCategoria] = useState('general');
+  const [noticias, setNoticias] = useState([]);
+  const [pagina, setPagina] = useState(1);
+  const [totalNoticias, setTotalNoticias] = useState(0);
+
+  const handleChangePagina = (e, valor) => {
+    setPagina(valor);
+  };
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const URL = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`;
+      const { data } = await axios(URL);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+      setPagina(1);
+    };
+    consultarAPI();
+  }, [categoria]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const URL = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&page=${pagina}&apiKey=${import.meta.env.VITE_API_KEY}`;
+      const { data } = await axios(URL);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+    };
+    consultarAPI();
+  }, [pagina]);
+
+  const handleChangeCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  return (
+    <NoticiasContext.Provider value={{ categoria, handleChangeCategoria, noticias, totalNoticias, handleChangePagina, pagina }}>
+      {children}
+    </NoticiasContext.Provider>
+  );
+};
+
+export { NoticiasProvider };
+
+export default NoticiasContext;
